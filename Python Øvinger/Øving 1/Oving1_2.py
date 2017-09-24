87e011eb84bcb29978b6439f8a3ab38f1dad7bf5
tsm121 <tsm121@gmail.com>
2017-09-24 16:01:29 +0200

    fixing structure

--- Python Øvinger/Øving 1/Oving1_2.py
+++
@@ -1,6 +0,0 @@
-name=input("Hva er ditt navn? ")
-born=int(input("Hvilket år er du født? "))
-alder=2015-born
-print ("Hei",name,"jeg også er",alder, "år gammel.")
-
-#Laget av Thomas Markussen
--- Python Øvinger/Øving 2/Oving2_9.py
+++
@@ -1,4 +0,0 @@
-#Regne ut farenheit til celsius
-
-farenheit = float(input("Skriv farenheit her:"))
-print((farenheit-32)/1.8)
