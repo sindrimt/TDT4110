e55013ed704ed8f2462331fb4f2b569530571b0b
tsm121 <tsm121@gmail.com>
2017-09-24 15:53:53 +0200

    fixing certain files

--- Forelesninger/16.sep.2015.py
+++
@@ -1,9 +0,0 @@
-a = float(input("Skriv inn ett tall \n"))
-if (a<10):
-    print("ditt tall er mindre enn 10")
-
-elif(a>10):
-    print("ditt tall er større enn 10")
-
-else:
-    print("ditt tall er 10.")
--- Forelesninger/bmi.py
+++
@@ -1,3 +0,0 @@
-def bmi(v,h):
-    svar = v/h**2
-    return svar
